6126d28feeebdfcdb949781a16e2690cea62fa4d
Andrea Pocorobba <a.pocorobba@betacom.it>
2020-02-11 10:41:21 +0100

    Commit before ddos protection

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const cookieparser = require('cookie-parser');
-const cors = require('cors');
-//var app = express();
-const body = require('body-parser');
+// const cors = require('cors');
 const app = express();
 const routes = require('./api');
+const helmet = require('helmet');
 
 const whiteListedOrigins = ['http://localhost:8080'];
 
 // Add all common middlewares
+app.use(helmet());
 app.use(express.json());
 
 // Enable Cross Origin Resource Sharing to all origins by default
-app.use(cors({
+/*app.use(cors({
   origin: (origin, callback) => {
     if (origin && whiteListedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
@@ -22,11 +22,10 @@ app.use(cors({
   },
   credentials: true,
   allowedHeaders: ['X-XSRF-Token', 'Content-Type'],
-}));
+}));*/
 
 app.use(cookieparser());
-app.use(body.json());
-app.use(body.urlencoded({ extended: false }));
-app.use(routes);
+app.use(express.urlencoded({ extended: false }));
+app.use('/api',routes);
 
 module.exports = app;
